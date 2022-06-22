ca1bfcc6d24745889a272c647a27fb592c07e91f
Anton Tovkach <atovk@icloud.com>
2022-06-22 19:06:58 +0400

    mod random_matrix

--- clockwise-matrix/clockwise_matrix_2.py
+++ clockwise-matrix/clockwise_matrix_2.py
@@ -8,26 +8,19 @@ def clockwise_matrix_2(matrix):
     offset_range = range(math.ceil(min(x, y) / 2))
 
     for offset in offset_range:
-
         # top
         for i in range(offset, x - offset):
             result.append(matrix[offset][i])
-
         # right
         for i in range(offset + 1, y - offset):
             result.append(matrix[i][-offset - 1])
-
         # bottom
         if (y - 2 * offset - 1 != 0):
             for i in reversed(range(offset, x - offset - 1)):
                 result.append(matrix[-offset - 1][i])
-
         # left
         if (x - 2 * offset - 1 != 0):
             for i in reversed(range(offset + 1, y - offset - 1)):
                 result.append(matrix[i][offset])
 
     return result
-
-
-# print(clockwise_matrix_2(MATRIX_7))
--- clockwise-matrix/random_matrix.py
+++ clockwise-matrix/random_matrix.py
@@ -1,26 +1,21 @@
 
+from tracemalloc import start
 import numpy as np
 import clockwise_matrix_2
-import clockwise_matrix_1
 
-for i in range(10):
-    m = np.random.randint(100,
-                          size=(np.random.randint(1, 5),
-                                np.random.randint(1, 5))
-                          ).tolist()
+for i in range(1):
+    matrix = np.random.randint(100,
+                            size=(np.random.randint(1, 5),
+                                  np.random.randint(1, 5))
+                            ).tolist()
 
-    l = []
-    for x in m:
-        l.extend(x)
+    matrix_1d = []
+    for x in matrix:
+        matrix_1d.extend(x)
 
-    cw_1 = clockwise_matrix_1.clockwise_matrix_1(m)
-    cw_2 = clockwise_matrix_2.clockwise_matrix_2(m)
+    cw_2 = clockwise_matrix_2.clockwise_matrix_2(matrix)
 
-    print(l)
-    print(cw_1)
+    print(matrix_1d, len(matrix[0]), len(matrix))
     print(cw_2)
 
-    assert len(l) == len(cw_1) == len(cw_2) and cw_1 == cw_2
-
-else:
-    print('OK')
+    
