7f5f33fc3da87aedca454b5919cb37e44a9f8380
Anton Tovkach <atovk@icloud.com>
2022-06-23 15:18:41 +0400

    ren x, y -> cols, rows

--- clockwise-matrix/clockwise_matrix_2.py
+++ clockwise-matrix/clockwise_matrix_2.py
@@ -3,24 +3,24 @@ import math
 
 def clockwise_matrix_2(matrix):
     result = []
-    x = len(matrix[0])
-    y = len(matrix)
-    offset_range = range(math.ceil(min(x, y) / 2))
+    columns = len(matrix[0])
+    rows = len(matrix)
+    offset_range = range(math.ceil(min(columns, rows) / 2))
 
     for offset in offset_range:
         # top
-        for i in range(offset, x - offset):
+        for i in range(offset, columns - offset):
             result.append(matrix[offset][i])
         # right
-        for i in range(offset + 1, y - offset):
+        for i in range(offset + 1, rows - offset):
             result.append(matrix[i][-offset - 1])
         # bottom
-        if (y - 2 * offset - 1 != 0):
-            for i in reversed(range(offset, x - offset - 1)):
+        if (rows - 2 * offset - 1 != 0):
+            for i in reversed(range(offset, columns - offset - 1)):
                 result.append(matrix[-offset - 1][i])
         # left
-        if (x - 2 * offset - 1 != 0):
-            for i in reversed(range(offset + 1, y - offset - 1)):
+        if (columns - 2 * offset - 1 != 0):
+            for i in reversed(range(offset + 1, rows - offset - 1)):
                 result.append(matrix[i][offset])
 
     return result
